ffcb029a1290f2e8f1549c321c8dae9469198878
Jameson McBride <mcbride.jameson@gmail.com>
2021-11-06 23:07:00 -0400

    Task: add tests for add, sub, mult, divide

---
+++ tests/__init__.py
@@ -0,0 +1,44 @@
+"""Testing the Calculator
+"""
+from calculator.main import Calculator
+
+def test_calculator_result():
+    """Tests to make sure calculator gives stored result.
+    """
+    calc = Calculator()
+    assert calc.result == 0
+
+def test_calculator_get_result():
+    """Tests the calculator's get_result method.
+    """
+    calc = Calculator()
+    assert calc.get_result() == 0
+
+def test_calculator_add():
+    """Tests calculator's add method.
+    """
+    calc = Calculator()
+    calc.add(5)
+    assert calc.get_result() == 5
+
+def test_calculator_subtract():
+    """Tests calculator's subtract method.
+    """
+    calc = Calculator()
+    calc.subtract(4)
+    assert calc.get_result() == -4
+
+def test_calculator_multiply():
+    """Tests calculator's multiplication method.
+    """
+    calc = Calculator()
+    calc.multiply(2, 2)
+    assert calc.get_result() == 4
+
+def test_calculator_division():
+    """Tests calculator's division method.
+    """
+    calc = Calculator()
+    calc.divide(4, 2)
+    assert calc.get_result() == 2
+
