eced1cd63a179ec19f4803ad291852d2f906ca60
Jameson McBride <mcbride.jameson@gmail.com>
2021-11-13 22:22:15 -0500

    Task: add multiplication class

---
+++ calc/multiplication.py
@@ -0,0 +1,13 @@
+from calc.calculation import Calculation
+
+class Multiplication(Calculation):
+    """Multiplication class.
+    """
+    
+    def get_result(self):
+        """Multiplies two numbers and returns result.
+
+        Returns:
+            int: result of multiplying two numbers
+        """
+        return self.value_a * self.value_b
