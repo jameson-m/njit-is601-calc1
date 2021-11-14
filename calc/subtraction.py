d89e481451baf1ce8f4570e6980a385a1aeed390
Jameson McBride <mcbride.jameson@gmail.com>
2021-11-13 22:22:07 -0500

    Task: add subtraction class

---
+++ calc/subtraction.py
@@ -0,0 +1,13 @@
+from calc.calculation import Calculation
+
+class Subtraction(Calculation):
+    """Subtraction class.
+    """
+    
+    def get_result(self):
+        """Subtracts two numbers together and returns result.
+
+        Returns:
+            int: result of subtracting one number from another
+        """
+        return self.value_a - self.value_b
