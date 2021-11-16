a70de2c9ab1a081165e91ed13118295a62203473
Jameson McBride <mcbride.jameson@gmail.com>
2021-11-15 21:31:43 -0500

    Task: add tests for more than two values

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,8 +1,6 @@
 """Testing the Calculator"""
 import pytest
 from calculator.main import Calculator
-from calc.addition import Addition
-from calc.calculation import Calculation
 
 @pytest.fixture(name="clear_history")
 def fixture_clear_history():
@@ -19,6 +17,10 @@ def test_calculator_add(clear_history):
     assert Calculator.count_history() == 4
     assert Calculator.get_last_calculation_result() == 6
 
+    # test more than 2 numbers
+    assert Calculator.add(1, 2, 3).get_result() == 6
+    assert Calculator.add(1, 1, 1, 1, 1, 1, 1, 1).get_result() == 8
+
 def test_calculator_subtract(clear_history):
     """Tests the calculator's subtract method."""
     assert clear_history is True
@@ -28,6 +30,10 @@ def test_calculator_subtract(clear_history):
     assert Calculator.subtract(6, 3).get_result() == 3
     assert Calculator.count_history() == 4
     assert Calculator.get_last_calculation_result() == 3
+
+    # test more than 2 numbers
+    assert Calculator.subtract(8, 3, 1).get_result() == 4
+    assert Calculator.subtract(16, 8, 1, 2).get_result() == 5
 
 def test_calculator_multiply(clear_history):
     """Tests the calculator's multiplication method."""
@@ -40,6 +46,11 @@ def test_calculator_multiply(clear_history):
     assert Calculator.count_history() == 5
     assert Calculator.get_last_calculation_result() == 4
 
+    # test more than 2 numbers
+    assert Calculator.multiply(2, 2, 2, 2).get_result() == 16
+    assert Calculator.multiply(1, 1, 1, 1, 1).get_result() == 1
+    assert Calculator.multiply(1, 2, 3).get_result() == 6
+
 def test_calculator_divide(clear_history):
     """Tests the calculator's division method."""
     assert clear_history is True
@@ -50,6 +61,11 @@ def test_calculator_divide(clear_history):
     assert Calculator.divide(2, 2).get_result() == 1
     assert Calculator.count_history() == 5
     assert Calculator.get_last_calculation_result() == 1
+
+    # test more than 2 numbers
+    assert Calculator.divide(16, 2, 2).get_result() == 4
+    assert Calculator.divide(1, 1, 1, 1, 1, 1, 1, 1).get_result() == 1
+    assert Calculator.divide(8, 3, 1, 0).get_result() == 0
 
 def test_calculator_divide_by_zero(clear_history):
     """Tests the calculator's ability to handle division by zero."""
@@ -87,11 +103,6 @@ def test_calculator_get_first_calculation(clear_history):
     first_calculation = Calculator.get_first_calculation()
     assert first_calculation.get_result() == 4
 
-def test_calculator_add_calculation_to_history(clear_history):
-    """Tests the calculator's add_calculation_to_history method."""
-    assert clear_history is True
-    assert Calculator.add_calculation_to_history(Addition.create(3, 2)) is True
-
 def test_calculator_get_last_calculation_result(clear_history):
     """Tests the calculator's get_last_calculation_result method."""
     assert clear_history is True
@@ -117,8 +128,3 @@ def test_calculator_count_history(clear_history):
     Calculator.add(3, 2)
     Calculator.add(3, 2)
     assert Calculator.count_history() == 5
-
-def test_calculation_speak(clear_history):
-    """Tests the calculation speak method."""
-    assert clear_history is True
-    assert Calculation.speak() == "Hello from your calculator!"
