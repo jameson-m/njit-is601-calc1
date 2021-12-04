242ce914b286e638931b9a4ab515ce93bc90e1c4
Jameson McBride <mcbride.jameson@gmail.com>
2021-12-04 14:14:52 -0500

    Task: refactor and simplify calc test data comparisons

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -15,65 +15,51 @@ def fixture_get_df():
     file_path = DataManager.get_absolute_path_from_relative_path("tests/test_data/test_data.csv")
     return DataManager.read_csv_data(file_path)
 
-def test_calculator_add(clear_history, df):
-    """Tests the calculator's add method."""
-    assert clear_history is True
-
-    addition_results_key = DataManager.get_addition_results(df)
-    values = DataManager.get_list_of_values(df)
-
-    for row_index, result_key in enumerate(addition_results_key):
+def run_calc_comparison(results, values, callback):
+    """Compares calculator's solution to results key."""
+    for row_index, result in enumerate(results):
         row_values = []
         for val_column in values:
             if val_column[row_index] is not None:
                 row_values.append(val_column[row_index])
 
-        assert Calculator.add(*row_values).get_result() == result_key
+        assert round(callback(*row_values).get_result(), 9) == result
+
+def test_calculator_add(clear_history, df):
+    """Tests the calculator's add method."""
+    assert clear_history is True
+
+    addition_results = DataManager.get_addition_results(df)
+    values = DataManager.get_list_of_values(df)
+
+    run_calc_comparison(addition_results, values, Calculator.add)
 
 def test_calculator_subtract(clear_history, df):
     """Tests the calculator's subtract method."""
     assert clear_history is True
 
-    subtraction_results_key = DataManager.get_subtraction_results(df)
+    subtraction_results = DataManager.get_subtraction_results(df)
     values = DataManager.get_list_of_values(df)
 
-    for row_index, result_key in enumerate(subtraction_results_key):
-        row_values = []
-        for val_column in values:
-            if val_column[row_index] is not None:
-                row_values.append(val_column[row_index])
-
-        assert Calculator.subtract(*row_values).get_result() == result_key
+    run_calc_comparison(subtraction_results, values, Calculator.subtract)
 
 def test_calculator_multiply(clear_history, df):
     """Tests the calculator's multiplication method."""
     assert clear_history is True
 
-    multiplication_results_key = DataManager.get_multiplication_results(df)
+    multiplication_results = DataManager.get_multiplication_results(df)
     values = DataManager.get_list_of_values(df)
 
-    for row_index, result_key in enumerate(multiplication_results_key):
-        row_values = []
-        for val_column in values:
-            if val_column[row_index] is not None:
-                row_values.append(val_column[row_index])
-
-        assert Calculator.multiply(*row_values).get_result() == result_key
+    run_calc_comparison(multiplication_results, values, Calculator.multiply)
 
 def test_calculator_divide(clear_history, df):
     """Tests the calculator's division method."""
     assert clear_history is True
 
-    division_results_key = DataManager.get_division_results(df)
+    division_results = DataManager.get_division_results(df)
     values = DataManager.get_list_of_values(df)
 
-    for row_index, result_key in enumerate(division_results_key):
-        row_values = []
-        for val_column in values:
-            if val_column[row_index] is not None:
-                row_values.append(val_column[row_index])
-
-        assert round(Calculator.divide(*row_values).get_result(), 9) == result_key
+    run_calc_comparison(division_results, values, Calculator.divide)
 
 def test_calculator_divide_by_zero(clear_history):
     """Tests the calculator's ability to handle division by zero."""
